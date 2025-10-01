38fc3d293623a7e81a472cd11a7a9c7a52198305
blednyikaklanaya <hootro34@gmail.com>
2025-10-01 22:51:58 +0300

    change

--- InstallMissingDrivers.ps1
+++ InstallMissingDrivers.ps1
@@ -21,6 +21,9 @@ $banner = @"
                \  |  /<`  (    )  `>
                 \ | /   `^-*==*-^`
                  \|/
+                 
+                    blednyikaklanaya make this
+                    
 "@
 Write-Host $banner -ForegroundColor Cyan
 
